2347bb0496be4956d08673cf5a2ad953255b78ac
Ekaterina Savoskina <korbanova20@gmail.com>
2023-06-19 23:00:04 +0300

    /project transfer to Angular

---
+++ collection-tea/src/app/app.component.ts
@@ -0,0 +1,74 @@
+import {Component, OnInit} from '@angular/core';
+ declare var $: any;
+//declare var jQuery: any;
+// import $ from 'jquery';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'collection-tea';
+  constructor() {
+    console.log('ssss');
+    // $('#accordion');
+  }
+  ngOnInit() {
+    console.log('init');
+    console.log($('#accordion'));
+    $('#accordion').accordion();
+    $('.btn').addClass(' hvr-grow');
+
+    $('.product-image').magnificPopup({
+      type: 'image'
+    });
+
+    let valIndex = $('#validationIndex');
+
+    // valIndex.on('keydown', (e) => {
+    //   if ((isNaN(parseInt(e.key)) || valIndex.val().length > 5) && e.key?.toLowerCase() !== 'backspace') {
+    //     return false;
+    //   }
+    // })
+
+    // function checkEmptyInput() {
+    //   let res = true;
+    //   $('.order input').each(function () {
+    //     let currentElem = $(this);
+    //     if (currentElem.val().trim() === '') {
+    //       alert('Необходимо заполнить поле ' + currentElem.prev().text());
+    //       currentElem.focus();
+    //       res = false;
+    //       return res;
+    //     }
+    //   });
+    //
+    //   return res;
+    // }
+
+   // $('#btnSend').on('click', (e) => {
+   //    let popUpSuccess = $('#popUpSuccess');
+   //    let orderForm = $('.order-form form');
+   //
+   //    if (!checkEmptyInput()) {
+   //      return false;
+   //    }
+   //
+   //    if (valIndex.val().length !== 6) {
+   //      alert('Поле индеск должно содержать 6 символов');
+   //      valIndex.focus();
+   //      return false;
+   //    }
+   //
+   //    popUpSuccess.css('height', orderForm.css('height'));
+   //    orderForm.addClass('d-none');
+   //    popUpSuccess.removeClass('d-none');
+   //  })
+  }
+
+  //console.log('jhjh');
+  // $('#accordion').accordion();
+
+
+}
